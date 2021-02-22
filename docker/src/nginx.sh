62e6e36ce4d2b375f496fd5b138cb77c5e99aef1
ThePooN <thepoon@osufr.live>
2021-02-22 17:58:56 +0100

    🔨 Make sure nginx config is generated before nginx first start

--- docker/src/nginx.sh
+++ docker/src/nginx.sh
@@ -3,6 +3,9 @@
 sed -i -E "s/^([\t| ]{0,})error_log.+$/\1error_log stderr warn;/" /etc/nginx/nginx.conf
 sed -i -E "s/^([\t| ]{0,})access_log.+$/\1access_log off;/"       /etc/nginx/nginx.conf
 
+echo 'Starting nginx config script...'
+node /srv/config.js
+
 echo 'Starting nginx...'
 nginx -g 'daemon off;' &
 PID=$!
