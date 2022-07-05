ba375a5e1b9e8c26f00087015d0b6878063ed7ca
ThePooN <thepoon@osufr.live>
2022-07-06 01:12:43 +0200

    🚑 Fix cache settings refresh

--- src/config.sh
+++ src/config.sh
@@ -5,7 +5,7 @@ node /srv/config.js
 
 # reload on config edit
 while true; do
-  inotifywait -e modify,move,create,delete /etc/proxy-config/virtualhosts.json /etc/proxy-config/secrets.json
+  inotifywait -e modify,move,create,delete /etc/proxy-config/cache.json /etc/proxy-config/virtualhosts.json /etc/proxy-config/secrets.json
     echo 'Regenerating nginx config following variables update...'
   node /srv/config.js
 done
